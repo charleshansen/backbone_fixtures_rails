93a03e82466ff203476a8a9345c7a464311a6b65
Charles Hansen & Kris Kelly <pair+chansen+kris@pivotallabs.com>
2013-06-20 15:29:37 -0700

    Rename some files

--- lib/backbone_rails_fixtures.rb
+++ lib/backbone_rails_fixtures.rb
@@ -1,3 +1,3 @@
-require 'fixture_generator'
+require 'fixture_json_generator'
 require 'fixture_middleware'
 require 'backbone_rails_fixtures/engine'
--- lib/generators/backbone_rails_fixtures/install_generator.rb
+++ lib/generators/backbone_rails_fixtures/install_generator.rb
@@ -2,12 +2,18 @@ module BackboneRailsFixtures
   module Generators
     class InstallGenerator < Rails::Generators::Base
       # Add stuff to jasmine.yml
-      # Add stuff to jasmine_helper.rb
 
       source_root File.expand_path("../templates", __FILE__)
 
       def create_fixture_definitions
         template "backbone_fixture_definitions.js", "spec/javascripts/helpers/backbone_fixture_definitions.js"
+      end
+
+      def instruct_to_add_jasmine_config
+        puts "Copy the following line(s) into your jasmine_helper.rb:\n"
+        puts "Jasmine.configure do |config|"
+        puts "  config.add_rack_path('/__fixtures', lambda { BackboneFixtures::FixtureMiddleware.new })"
+        puts "end"
       end
     end
   end
