f728352394526fe3ef2bc09ae3e912952a43b4b9
Charles Hansen <chansen87@gmail.com>
2014-02-09 18:14:14 -0800

    Fix load path for fixture middleware in jasmine_helper

--- spec/javascripts/support/jasmine_helper.rb
+++ spec/javascripts/support/jasmine_helper.rb
@@ -1,4 +1,4 @@
-require 'lib/fixture_middleware'
+require File.join(File.dirname(__FILE__), '..', '..', '..', 'lib', 'fixture_middleware')
 
 Jasmine.configure do |config|
   config.add_rack_path('/__fixtures', lambda { BackboneFixtures::FixtureMiddleware.new })
